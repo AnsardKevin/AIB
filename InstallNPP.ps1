8f42f17ba184f735efacfd37c597c7313acf77d8
Kévin ANSARD <kevin.ansard@infeeny.com>
2021-03-17 10:35:49 +0100

    push script to install npp

---
+++ InstallNPP.ps1
@@ -0,0 +1,15 @@
+﻿#region Set logging
+
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message"
+}
+#endregion
+
+try {
+    Start-Process -filepath 'c:\temp\npp.7.9.4.Installer.exe' -Wait -ErrorAction Stop -ArgumentList '/S'
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error installing Notepad++: $ErrorMessage"
+}
